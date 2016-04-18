24330691cdacc2cda09ca0452d620a678e216d8c
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-18 17:09:12 +0100

    Can add and remove toDos

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,3 +1,17 @@
 toDoApp.controller('ToDoController', function() {
-  this.todos = ["ToDo1","ToDo2","ToDo3"];
+
+  var self = this;
+
+  self.todos = [{ text: "ToDo1", completed: true },
+                { text: "ToDo2", completed: false},
+                { text: "ToDo3", completed: false }];
+
+  self.addToDo = function(toDo) {
+    self.todos.push({text: toDo, completed: false});
+  };
+
+  self.removeToDo = function(toDo) {
+    index = self.todos.indexOf(toDo);
+    self.todos.splice(index,1);
+  };
 });
